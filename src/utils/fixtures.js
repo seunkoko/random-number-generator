333f6433eec69418118328c84d5c4f94e37ea64f
seunkoko <seunowonikoko@gmail.com>
2018-10-02 23:20:45 +0100

    Handling Files: - Read from file - Save to file - Update Tests - Eject CRA

--- src/utils/fixtures.js
+++ src/utils/fixtures.js
@@ -1,4 +1,4 @@
-export const dummyGeneratedNumbers = [
+export const dummyGeneratedNumbers = [ // eslint-disable-line
   { id: 2, value: 2222222222 }, { id: 2, value: 1222222222 }, { id: 1, value: 2222222222 }, // eslint-disable-line
   { id: 2, value: 22211122222 }, { id: 2, value: 2222222222 }, { id: 2, value: 22445522222 }, // eslint-disable-line
   { id: 2, value: 2122222222 }, { id: 2, value: 2222222222 }, { id: 2, value: 2222222222 }, // eslint-disable-line
@@ -30,5 +30,3 @@ export const dummyGeneratedNumbers = [
   { id: 2, value: 2222222222 }, { id: 2, value: 2222222222 }, { id: 2, value: 2222222222 }, // eslint-disable-line
   { id: 2, value: 2222222222 }, { id: 2, value: 2222222222 }, { id: 2, value: 2222222222 }, // eslint-disable-line
 ];
-
-export const emptyGeneratedNumbers = [];
